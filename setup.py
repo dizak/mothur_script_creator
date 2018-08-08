48fbd9cec6e012e7ee973b9a525138042e4f46b9
dizak <dariusz.izak@ibb.waw.pl>
2018-08-08 11:37:28 +0200

    UPD: Using distutils. UPD: data_files must be specified for the installtion. UPD: Using __author and __version as modules.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import find_packages
+from distutils.core import setup
 from mothulity import __version__ as VERSION
 from mothulity import __author__ as AUTHOR
 
@@ -9,14 +10,21 @@ setup(
     author=AUTHOR,
     packages=find_packages(exclude=["*test*"]),
     include_package_data=True,
+    data_files=[
+        ("bin", ["mothulity.config"])
+    ],# NOTE: tells where files should be installed - see https://docs.python.org/3/distutils/setupscript.html#installing-additional-files
     install_requires=open("requirements.txt").readlines(),
     description="Easy-to-use tool facilitating work with Mothur.",
     long_description=open("README.md").read(),
-    # long_description_content_type="text/markdown",
+    long_description_content_type="text/markdown",
     author_email="dariusz.izak@ibb.waw.pl",
     url="https://github.com/dizak/mothulity",
     license="BSD",
-    py_modules=["utilities"],
+    py_modules=[
+        "__author",
+        "__version",
+        "utilities",
+    ],
     scripts=[
         "mothulity.py",
         "mothulity_draw.py",
